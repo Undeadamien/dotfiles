f2d47d48190683cbdde2a82e8eb0acbeccf492e7
undeadamien <damien.trala@outlook.com>
2024-11-04 20:12:30 +0000

    first working commit

--- setup.sh
+++ setup.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+for file in $(find "home" -type f); do
+	link="${HOME}/${file#home/}"
+	mkdir -p "$(dirname "$link")"
+
+	if [[ -e "$link" ]] && [[ $(diff "$file" "$link") ]]; then
+		diff "$file" "$link"
+		read -p "Do you want to replace the file (y/n): " res
+		if [[ "$res" =~ ^[yY]$ ]]; then
+			printf "Replacing the file %s\n" $link
+			ln -sf "$PWD/$file" "$link"
+		else
+			printf "Skipping the file %s\n" $link
+		fi
+	elif [[ -e "$link" ]]; then
+		printf "Replacing the file %s\n" $link
+		ln -sf "$PWD/$file" "$link"
+	else
+		printf "Creating the file %s\n" $link
+		ln -s "$PWD/$file" "$link"
+	fi
+done
