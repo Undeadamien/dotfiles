5a8854e0b28d42b4affee4ebdd9323282c0fb0f5
undeadamien <damien.trala@outlook.com>
2025-05-26 17:32:25 +0200

    working window finder

--- hypr/dot-config/hypr/script/window_finder.sh
+++ hypr/dot-config/hypr/script/window_finder.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
-windows="$(hyprctl clients -j | jq -c '.[]')"
-classes="$(echo "${windows}" | jq -cr '.class')"
-titles="$(echo "${windows}" | jq -cr '.title')"
-addresses="$(echo "${windows}" | jq -cr '.address')"
+entries="$(
+	hyprctl clients -j |
+		jq -c '.[] | [.class,.title,.address]'
+)"
+formatted="$(
+	echo "$entries" |
+		jq -cr '"\(.[0])\t\"\(.[1])\""' |
+		column -t -o ' | ' -s $'\t'
+)"
+selected="$(
+	echo "${formatted}" |
+		rofi -dmenu -i -p "Select window" -format d -show-icons=false
+)"
+target="$(
+	echo "$entries" |
+		sed -n "${selected}p" |
+		jq -cr '.[2]'
+)"
 
-echo "$classes"
-echo "$titles"
-echo "$addresses"
-
-exit
-
-selected="$(echo "${entries}" | rofi -dmenu -i -p "Select window")"
-
-selected_title="$(echo "${selected}" | awk -F'|' '{print $2}' | sed 's@\(^\s*\|\s*$\)@@g')"
-selected_window="$(echo "${windows}" | jq --arg title "$selected_title" -c 'select(.title == $title)')"
-selected_pid="$(echo "${selected_window}" | jq '.pid')"
-
-hyprctl dispatcher focuswindow pid:"${selected_pid}"
+hyprctl dispatcher focuswindow address:"${target}"
