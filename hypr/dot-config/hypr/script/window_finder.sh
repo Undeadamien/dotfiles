0d9f9bfcc2c807a100ff8fe7fdb12e476a59b453
undeadamien <damien.trala@outlook.com>
2025-05-27 12:55:15 +0200

    rofi show-icons to "false" and sorted window_finder with workspaces

--- hypr/dot-config/hypr/script/window_finder.sh
+++ hypr/dot-config/hypr/script/window_finder.sh
@@ -2,13 +2,15 @@
 
 entries="$(
 	hyprctl clients -j |
-		jq -c '.[] | [.class,.title,.address]'
+		jq -c '. | sort_by(.workspace.name) | .[] | [.class,.title,.address,.workspace.name]'
 )"
+echo "$entries"
 formatted="$(
 	echo "$entries" |
-		jq -cr '"\(.[0])\t\"\(.[1])\""' |
-		column -t -o ' | ' -s $'\t'
+		jq -cr '"\(.[3])\t\(.[0])\t\"\(.[1])\""' |
+		column -t -o $'\t| ' -s $'\t'
 )"
+echo "$formatted"
 selected="$(
 	echo "${formatted}" |
 		rofi -dmenu -i -p "Select window" -format d -show-icons=false
