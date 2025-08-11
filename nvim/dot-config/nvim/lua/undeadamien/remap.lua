e2afaf345ed7a8268d7aafeeb5f8a3892340494e
Undeadamien <damien.trala@outlook.com>
2025-08-11 11:27:21 +0200

    added some remap to delete into the void register

--- nvim/dot-config/nvim/lua/undeadamien/remap.lua
+++ nvim/dot-config/nvim/lua/undeadamien/remap.lua
@@ -1,11 +1,21 @@
 --Leader
 vim.g.mapleader = " "
 
---Copy and Paste
+--Copy
 vim.keymap.set("n", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", '"+yg_')
 vim.keymap.set("v", "<leader>y", '"+y')
+
+--Paste
 vim.keymap.set("n", "<leader>p", '"+p')
 vim.keymap.set("n", "<leader>P", '"+P')
 vim.keymap.set("v", "<leader>p", '"+p')
 vim.keymap.set("v", "<leader>P", '"+P')
+
+--Delete
+vim.keymap.set("n", "<leader>d", '"_d')
+vim.keymap.set("v", "<leader>d", '"_d')
+vim.keymap.set("n", "<leader>D", '"_D')
+vim.keymap.set("v", "<leader>D", '"_D')
+vim.keymap.set("n", "<leader>x", '"_x')
+vim.keymap.set("v", "<leader>x", '"_x')
