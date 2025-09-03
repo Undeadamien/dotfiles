dd68a04f241a5199a123b4c8de11cf43a5579750
Undeadamien <damien.trala@outlook.com>
2025-09-03 22:07:29 +0200

    wallpaper cycle

---
+++ hypr/dot-config/hypr/script/wallpaper_selector.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+pgrep -fx "$(realpath "$0")" | grep -vw "$$" | xargs -r kill
+
+current="$HOME/.config/hypr/wallpaper/wallpaper.jpg"
+wallpaper_dir="$HOME/.config/hypr/wallpaper/"
+
+wallpapers="$(find "${wallpaper_dir}" -not -name 'wallpaper.jpg' -type f,l)"
+interval=600
+
+if ! pgrep -x swww-daemon >/dev/null; then
+	hyprctl dispatch exec swww-daemon
+fi
+
+while true; do
+	if ! pgrep -x hyprlock >/dev/null; then
+		wallpapers="$(find "${wallpaper_dir}" -not -name 'wallpaper.jpg' -type f,l)"
+		selected="$(echo "$wallpapers" | shuf -n1)"
+		cp "$selected" "$current"
+		swww img "$current"
+	fi
+	sleep "$interval"
+done
