97d96210d9d623b635cee4c46257ccbd7cbdcff3
Undeadamien <damien.trala@outlook.com>
2025-12-14 12:47:57 +0100

    hyprland: fixed the wallpaper script life cycle

--- dot_config/hypr/script/executable_wallpaper_selector.sh
+++ dot_config/hypr/script/executable_wallpaper_selector.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 
 current="wallpaper.jpg"
 interval=600
 wallpaper_dir="$HOME/.config/hypr/wallpaper/"
-wallpapers="$(find "${wallpaper_dir}" -not -name "${current}" -type f,l)"
 
 if ! pgrep -x swww-daemon >/dev/null; then
 	hyprctl dispatch exec swww-daemon
 fi
 
+last_change=$(date +%s)
+
 while true; do
-	if ! pgrep -x hyprlock >/dev/null; then
+	if ! pgrep -x Hyprland >/dev/null; then
+		break
+	fi
+	if pgrep -x hyprlock >/dev/null; then
+		sleep 1
+		continue
+	fi
+
+	now=$(date +%s)
+	elapsed=$((now - last_change))
+	if [ "$elapsed" -ge "$interval" ]; then
 		wallpapers="$(find "${wallpaper_dir}" -not -name "${current}" -type f)"
 		selected="$(echo "$wallpapers" | shuf -n1)"
 		cp "$selected" "${wallpaper_dir}/${current}"
@@ -20,6 +30,7 @@ while true; do
 			--transition-duration 8 \
 			--transition-fps 60 \
 			"${wallpaper_dir}/${current}"
+		last_change=$now
 	fi
-	sleep "$interval"
+	sleep 1
 done
