537c06df2dd101d79a64a4a91228b16ef0de9078
Undeadamien <damien.trala@outlook.com>
2025-12-14 19:02:24 +0100

    wallpapers moved into subdirs, and improved wallpaper script

--- dot_config/hypr/script/executable_wallpaper_selector.sh
+++ dot_config/hypr/script/executable_wallpaper_selector.sh
@@ -9,6 +9,18 @@ if ! pgrep -x swww-daemon >/dev/null; then
 	hyprctl dispatch exec swww-daemon
 fi
 
+if ! pgrep -x Hyprland >/dev/null; then
+	exit 1
+fi
+
+change_wallpaper() {
+	wallpapers="$(find "${wallpaper_dir}" -not -name "${current}" -type f)"
+	selected="$(echo "$wallpapers" | shuf -n1)"
+	cp "$selected" "${wallpaper_dir}/${current}"
+	swww img --transition-duration 8 --transition-fps 60 "${wallpaper_dir}/${current}"
+}
+
+change_wallpaper
 last_change=$(date +%s)
 
 while true; do
@@ -23,13 +35,7 @@ while true; do
 	now=$(date +%s)
 	elapsed=$((now - last_change))
 	if [ "$elapsed" -ge "$interval" ]; then
-		wallpapers="$(find "${wallpaper_dir}" -not -name "${current}" -type f)"
-		selected="$(echo "$wallpapers" | shuf -n1)"
-		cp "$selected" "${wallpaper_dir}/${current}"
-		swww img \
-			--transition-duration 8 \
-			--transition-fps 60 \
-			"${wallpaper_dir}/${current}"
+		change_wallpaper
 		last_change=$now
 	fi
 	sleep 1
