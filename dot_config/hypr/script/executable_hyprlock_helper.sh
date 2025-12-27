6360d2074f5b8edba2f5fc99c97ce04dd27e606a
Undeadamien <damien.trala@outlook.com>
2025-12-27 22:03:50 +0100

    hyprlock: changed the approach for muting

--- dot_config/hypr/script/executable_hyprlock_helper.sh
+++ dot_config/hypr/script/executable_hyprlock_helper.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-IS_MUTED=$(wpctl get-volume @DEFAULT_SINK@ | grep -c MUTED)
+VOLUME=$(wpctl get-volume @DEFAULT_SINK@ | awk '{printf $2}')
 
 playerctl --all-players pause
-wpctl set-mute @DEFAULT_SINK@ 1
+wpctl set-volume @DEFAULT_SINK@ 0
 hyprlock
-wpctl set-mute @DEFAULT_SINK@ $IS_MUTED
+wpctl set-volume @DEFAULT_SINK@ $VOLUME
