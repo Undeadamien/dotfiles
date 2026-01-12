aaf9c3106a1f2952af2e5e9ed4adfa74e90ad16c
Undeadamien <damien.trala@outlook.com>
2026-01-12 13:14:10 +0100

    hyprshot: removed the --freeze option that conflict with rofi animations

--- dot_config/hypr/script/executable_screenshot.sh
+++ dot_config/hypr/script/executable_screenshot.sh
@@ -4,12 +4,12 @@ option=$(echo -e "screen\nwindow\narea" | rofi -dmenu -p "Screenshot")
 
 case "$option" in
 "screen")
-	hyprshot -m output -m active --freeze
+	hyprshot -m output -m active
 	;;
 "window")
-	hyprshot -m window --freeze
+	hyprshot -m window
 	;;
 "area")
-	hyprshot -m region --freeze
+	hyprshot -m region
 	;;
 esac
