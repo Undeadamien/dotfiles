cdc124b3a2b77daa649d2be04a1c496d172cec4f
Undeadamien <damien.trala@outlook.com>
2026-01-13 18:01:51 +0100

    chezmoi: cleanup and 'run_onchange_*' for packages

--- ignored/yay.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$HOME"
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
-rm -rf yay-bin
---
+++ run_onchange_install_packages.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+PACKAGES_PACMAN=(
+	android-file-transfer
+	bat
+	brightnessctl
+	btop
+	chezmoi
+	cronie
+	discord
+	firefox
+	fzf
+	hypridle
+	hyprland
+	hyprlock
+	hyprpaper
+	hyprshot
+	iwd
+	krita
+	ly
+	mpc
+	mpd
+	neovim
+	obsidian
+	pavucontrol
+	qbittorrent
+	ripgrep
+	rmpc
+	rofi
+	steam
+	swaync
+	swww
+	syncthing
+	thunar
+	tree
+	ttf-ubuntu-font-family
+	ttf-ubuntu-mono-nerd
+	ttf-ubuntu-nerd
+	vlc
+	waybar
+	wev
+	wget
+	wlsunset
+	zsh-autocomplete
+)
+PACKAGES_YAY=(
+	opera
+	xp-pen-tablet
+)
+
+echo "Updating pacman databases..."
+sudo pacman -Syy
+sudo pacman -Syu
+
+echo "Installing pacman packages..."
+sudo pacman -S --needed --noconfirm "${PACKAGES_PACMAN[@]}"
+
+if ! command -v yay &>/dev/null; then
+	echo "Installing yay..."
+	sudo pacman -S --needed --noconfirm base-devel git
+	git clone https://aur.archlinux.org/yay.git /tmp/yay
+	(cd /tmp/yay && makepkg --si --noconfirm)
+	rm -rf /tmp/yay
+fi
+
+echo "Installing AUR packages..."
+yay -S --needed --noconfirm "${PACKAGES_YAY[@]}"
+
+echo "Installation done."
