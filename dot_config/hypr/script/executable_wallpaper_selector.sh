b5a1e00ed9748816864fccae2d3d25c0b982e882
Undeadamien <damien.trala@outlook.com>
2026-03-25 17:03:34 +0100

    improve wallpaper_selector.sh

--- dot_config/hypr/script/executable_wallpaper_selector.sh
+++ dot_config/hypr/script/executable_wallpaper_selector.sh
@@ -2,21 +2,21 @@
 
 set -euo pipefail
 
+wallpaper_dir="${HOME}/.config/hypr/wallpaper/"
 current="wallpaper"
-wallpaper_dir="$HOME/.config/hypr/wallpaper/"
+fps=60
+speed=8
 
 manual_select() {
     selection="$(
-        for img in "$wallpaper_dir"/*; do
-            [ "$(basename "$img")" = "$current" ] && continue
-            echo -en "$(basename "$img")\0icon\x1f$img\n"
+        for img in "${wallpaper_dir}"/*; do
+            [ "$(basename "${img}")" = "${current}" ] && continue
+            echo -en "$(basename "${img}")\0icon\x1f${img}\n"
         done | rofi -dmenu -theme wallpaper.rasi -p "wallpapers" || true
     )"
-    if [[ -z "$selection" ]] || [[ ! -e "$wallpaper_dir/$selection" ]]; then
-        return
-    fi
-    ln -sf "$wallpaper_dir/$selection" "$wallpaper_dir/$current"
-    awww img "$wallpaper_dir/$current" --transition-duration 4 --transition-type center --transition-fps 60
+    if [[ -z "${selection}" ]] || [[ ! -e "${wallpaper_dir}/${selection}" ]]; then return; fi
+    ln -sf "${wallpaper_dir}/${selection}" "${wallpaper_dir}/${current}"
+    awww img "${wallpaper_dir}/${current}" --transition-duration "${speed}" --transition-type fade --transition-fps "${fps}"
 }
 
 if ! pgrep -x awww-daemon >/dev/null; then hyprctl dispatch exec awww-daemon; fi
@@ -28,7 +28,7 @@ if [[ "${1:-}" == "select" ]]; then
 fi
 
 wallpapers="$(find "${wallpaper_dir}" -not -name "${current}" -type f)"
-selected="$(echo "$wallpapers" | shuf -n1)"
-if [ -z "$selected" ]; then exit 0; fi
-ln -sf "$selected" "${wallpaper_dir}/${current}"
-awww img --transition-duration 8 --transition-fps 60 "${wallpaper_dir}/${current}"
+selected="$(echo "${wallpapers}" | shuf -n1)"
+if [ -z "${selected}" ]; then exit 0; fi
+ln -sf "${selected}" "${wallpaper_dir}/${current}"
+awww img --transition-duration "${speed}" --transition-fps "${fps}" "${wallpaper_dir}/${current}"
